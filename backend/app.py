1b13559f1af8794aa6370ec3f45d5e657572931b
kemokaba <kemokaba@gmail.com>
2023-02-24 09:40:27 +0100

    recherche

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,29 @@
-import requests
+import requests, jsonify, request
 from flask import *
+import re
 
-app = Flask('mars_discovery')
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:passer1234@localhost/books'
+app.app_context().push()
+db = SQLAlchemy(app)
 
+# Models
+class Livre(db.Model):
+    __tablename__='livres'
+    id = db.Column(db.Integer, primary_key=True)
+    titre = db.Column(db.String(80), unique=True, nullable=False)
+    contenu = db.Column(db.Text, unique=True, nullable=False)
+    auteur = db.Column(db.String(), unique=True)
+    date_pub = db.Column(db.DateTime)
 
-@app.route('/')
+    def __repr__(self):
+        return f'<Livre {self.contenu}>'
+#db.create_all()
+
+db.session.commit()
+
+@app.route('/',methods=['GET'])
 def index():
     books = []
     if 'book' in request.args:
@@ -15,9 +34,14 @@ def index():
     return render_template('acceuil.html', books=books)
 
 
+
 @app.route('/home')
 def home():
     return render_template('acceuil.html')
 
 
-app.run(debug=True)
+
+
+
+if __name__ == '__main__':
+    app.run()
